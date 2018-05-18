102232fb9d08e4dc48cbeaa485ce7bd0542fd978
MSalah <mohammedsalaah96@gmail.com>
2018-05-18 15:28:49 +0200

    Done

---
+++ driver/src/main/java/com/graduationproject/moham/driver/Screens/MainActivity.java
@@ -0,0 +1,15 @@
+package com.graduationproject.moham.driver.Screens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.graduationproject.moham.driver.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
